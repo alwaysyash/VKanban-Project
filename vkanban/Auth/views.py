0ad6780f9e78dc958153b0ed54acfc77448d07bc
Alok Joshi <AlokJoshi523@gmail.com>
2022-11-23 12:18:43 +0530

    Basic implementation of Auth done

--- vkanban/Auth/views.py
+++ vkanban/Auth/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.urls.resolvers import re
+import pdb
 
-# Create your views here.
+
+def Login(request):
+
+    if(request.method == "GET"):
+        return render(request,"Auth/login.html",{})
+    elif(request.method == "POST"):
+
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        user = authenticate(request,email = email,password = password)
+        if(user is not None):
+            login(request,user)
+            #redirect to main page
+        else:
+            #render(request,"auth/login.html",{}) with context containing wrong password
+            pass
+
+
+
+
+
+def registration(request):
+    if(request.method == "GET"):
+        return  render(request,"Auth/register.html",{})
+
+    elif(request.method == "POST"):
+        
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        
+
