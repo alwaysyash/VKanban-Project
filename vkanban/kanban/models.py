8a2d49cada81910cffdfd166600a3e6727087690
Shreyas Gavhalkar <shreyas@pop-os.localdomain>
2022-12-01 22:09:15 +0530

    added the models according to ER diagram

--- vkanban/kanban/models.py
+++ vkanban/kanban/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Kanban(models.Model):
+    description = models.CharField(max_length=1000)
+    KanbanName = models.CharField(max_length=200)
+    KanbanId = models.IntegerField(unique=True, primary_key=True)
+
+
+class Cards(models.Model):
+
+    cardID = models.IntegerField()
+    KanbanId = models.ForeignKey(Kanban, on_delete=models.PROTECT)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    state = models.TextField(max_length=200)
+    last_edit = models.DateTimeField()
+    content = models.TextField(max_length=1000)
+    deadline = models.DateTimeField()
+    class Meta:
+        unique_together = (('cardID', 'KanbanId'))
+
+class creates(models.Model):
+    card = models.ForeignKey(Cards, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+class hasAcessTo(models.Model):
+    kanban = models.ForeignKey(Kanban, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+# class contains(models.Model):
--- vkanban/kanban/views.py
+++ vkanban/kanban/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
+@login_required
+def home(request):
+    pass
